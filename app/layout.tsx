638b94ed3648e479ba16784a823cac49882c5b74
karticklalwani <kartikzzler@hotmail.com>
2025-12-14 21:02:22 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,14 @@
+import { ReactNode } from "react";
+import { UserProvider } from "@/providers/UserProvider";
 
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="es">
+      <body>
+        <UserProvider>
+          {children}
+        </UserProvider>
+      </body>
+    </html>
+  );
+}
