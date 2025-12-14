d8a71e3260829dd2ccbd17aca3ccb4443dc13d42
karticklalwani <kartikzzler@hotmail.com>
2025-12-14 21:43:33 +0000

    Create middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+  const supabase = createMiddlewareClient({ req, res });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  const isLoginPage = req.nextUrl.pathname.startsWith("/login");
+
+  if (!session && !isLoginPage) {
+    const redirectUrl = req.nextUrl.clone();
+    redirectUrl.pathname = "/login";
+    return NextResponse.redirect(redirectUrl);
+  }
+
+  return res;
+}
+
+export const config = {
+  matcher: ["/((?!_next|favicon.ico).*)"],
+};
