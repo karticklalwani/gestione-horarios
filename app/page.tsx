35cc35f03a2aa3dd42bee594e34b46510a9b07ab
karticklalwani <kartikzzler@hotmail.com>
2025-12-15 21:46:40 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,3 @@
-"use client";
-
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { useUser } from "@/providers/UserProvider";
-
-export default function HomePage() {
-  const { user, role, loading } = useUser();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (loading) return;
-
-    if (!user) {
-      router.push("/login");
-      return;
-    }
-
-    if (role === "superadmin") {
-      router.push("/dashboard");
-    } else if (role === "admin") {
-      router.push("/tiendas");
-    } else if (role === "empleado") {
-      router.push("/mis-horarios");
-    }
-  }, [user, role, loading, router]);
-
-  return <p>Cargando...</p>;
+export default function Home() {
+  return <h1>FUNCIONA MI APP</h1>;
 }
